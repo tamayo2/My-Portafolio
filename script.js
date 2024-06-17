705b9c1713f1743d3f1e3cb089414e1d29464f75
tamayo2 <juan.tamayo3@utp.edu.co>
2024-06-17 17:11:28 -0500

    Modal

--- script.js
+++ script.js
@@ -20,6 +20,29 @@ window.onscroll = () => {
 }
 
 
+// Obtener el modal
+var modal = document.getElementById("myModal");
+
+// Obtener el elemento <span> que cierra el modal
+var span = document.getElementsByClassName("close")[0];
+
+// Cuando la página se carga, mostrar el modal
+window.onload = function() {
+    modal.style.display = "block";
+}
+
+// Cuando el usuario haga clic en <span> (x), cerrar el modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// Cuando el usuario haga clic en cualquier lugar fuera del modal, cerrarlo
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
